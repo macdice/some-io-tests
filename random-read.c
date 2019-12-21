bd2b63a703cefeffb6a65940495c1268c7ac6235
Thomas Munro <thomas.munro@gmail.com>
2019-12-22 01:20:50 +1300

    Fix random seed initialisaiton.  Add bogus work loop.

--- random-read.c
+++ random-read.c
@@ -6,24 +6,29 @@
 
 #define BLOCK_SIZE 8192
 
+/* make this global so the bogus work loop doesn't get optimised away */
+int checksum;
+
 int main(int argc, char *argv[])
 {
 	off_t *queue;
 	int distance;
 	int slots;
 	int nreads;
+	int work;
 	int fd;
 	off_t file_size;
 	size_t blocks;
 
-	if (argc != 4) {
-		printf("usage: %s path distance nreads\n", argv[0]);
+	if (argc != 5) {
+		printf("usage: %s path distance nreads work\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
 	distance = atoi(argv[2]);
 	slots = distance == 0 ? 1 : distance;
 	nreads = atoi(argv[3]);
+	work = atoi(argv[4]);
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0) {
 		perror("open");
@@ -38,7 +43,7 @@ int main(int argc, char *argv[])
 	blocks = file_size / BLOCK_SIZE;
 
 	/* we want different random numbers for every run */
-	srand(time(NULL));
+	srandom(time(NULL));
 
 	/* create the queue (plus 1 because we want 1 slot if 0) */
 	queue = malloc(sizeof(off_t) * (distance == 0 ? 1 : distance));
@@ -70,12 +75,15 @@ int main(int argc, char *argv[])
 		/* replace this slot with a new random block */
 		queue[slot] = (random() % blocks) * BLOCK_SIZE;
 
-		/* start prefetching */
+		/* prefetch one more */
 		if (distance > 0 && posix_fadvise(fd, queue[slot], BLOCK_SIZE, POSIX_FADV_WILLNEED) != 0) {
 			perror("posix_fadvise");
 			return EXIT_FAILURE;
 		}
-	}
 
-	close(fd);
+		/* compute a bogus checksum as a kind of per page 'work' */
+		for (int j = 0; j < work; ++j) {
+			checksum += buffer[j % BLOCK_SIZE];
+		}
+	}
 }
