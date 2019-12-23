d4f4dca60f680c3d4eb81a11a5ffccab67ca6743
Thomas Munro <thomas.munro@gmail.com>
2019-12-23 16:35:25 +1300

    Add backwards seq read test.

--- sequential-read.c
+++ sequential-read.c
@@ -8,16 +8,18 @@
 
 int main(int argc, char *argv[])
 {
-	int fadvise;
+	int direction;
+	int distance;
 	int fd;
 	off_t file_size;
 
-	if (argc != 3) {
-		printf("usage: %s path fadvise\n", argv[0]);
+	if (argc != 4) {
+		printf("usage: %s path distance direction\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	fadvise = atoi(argv[2]);
+	distance = atoi(argv[2]);
+	direction = atoi(argv[3]);
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0) {
@@ -31,18 +33,41 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	for (off_t offset = 0; offset < file_size; offset += BLOCK_SIZE) {
-		char buffer[BLOCK_SIZE];
+	if (direction > 0) {
+		for (off_t offset = 0; offset < file_size; offset += BLOCK_SIZE) {
+			char buffer[BLOCK_SIZE];
+			off_t prefetch_offset;
 
-		if (fadvise) {
-			if (posix_fadvise(fd, offset, BLOCK_SIZE, POSIX_FADV_WILLNEED) != 0) {
-				perror("posix_fadvise");
+			if (pread(fd, buffer, BLOCK_SIZE, offset) != BLOCK_SIZE) {
+				perror("pread");
 				return EXIT_FAILURE;
 			}
+
+			prefetch_offset = offset + BLOCK_SIZE * distance;
+			if (distance > 0 && prefetch_offset < file_size) {
+				if (posix_fadvise(fd, prefetch_offset, BLOCK_SIZE, POSIX_FADV_WILLNEED) != 0) {
+					perror("posix_fadvise");
+					return EXIT_FAILURE;
+				}
+			}
 		}
-		if (pread(fd, buffer, BLOCK_SIZE, offset) != BLOCK_SIZE) {
-			perror("pread");
-			return EXIT_FAILURE;
+	} else {
+		for (off_t offset = file_size - BLOCK_SIZE; offset >= 0; offset -= BLOCK_SIZE) {
+			char buffer[BLOCK_SIZE];
+			off_t prefetch_offset;
+
+			if (pread(fd, buffer, BLOCK_SIZE, offset) != BLOCK_SIZE) {
+				perror("pread");
+				return EXIT_FAILURE;
+			}
+
+			prefetch_offset = offset - BLOCK_SIZE * distance;
+			if (distance > 0 && prefetch_offset >= 0) {
+				if (posix_fadvise(fd, prefetch_offset, BLOCK_SIZE, POSIX_FADV_WILLNEED) != 0) {
+					perror("posix_fadvise");
+					return EXIT_FAILURE;
+				}
+			}
 		}
 	}
 }
